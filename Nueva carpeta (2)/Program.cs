af440a2e4e2c3400234e2c0b1b92def9f2c92751
EfrainMartinezLeyva <efrainybetsi@gmail.com>
2025-02-24 11:45:58 -0700

    Add files via upload

---
+++ Nueva carpeta (2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace ListasSimplementeLigadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista lista = new Lista();
+
+            lista.Agregar("A");
+
+            lista.Agregar("B");
+            lista.Agregar("C");
+            lista.Agregar("D");
+            lista.Agregar("E");
+
+            Console.WriteLine(lista.ObtenerValores());
+
+            lista.Eliminar("C");
+
+            Console.WriteLine();
+            Console.WriteLine(lista.ObtenerValores());
+        }
+    }
+}
